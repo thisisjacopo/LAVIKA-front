f9f98f1fe89c78f63433286b0b2483a1549d8900
Jacopo <jacopo.rodighiero@icloud.com>
2020-05-21 15:28:54 +0200

    thursday jacopo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
-import Sketches from './components/Sketches'
-import NewSketch from './components/NewSketch'
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Private from "./pages/Private";
+import NewSketch from "./components/NewSketch";
+import PublicRoute from "./components/PublicRoute";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
-    <NewSketch />
+      <Navbar />
+      {/* <NewSketch /> */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <PublicRoute exact path="/signup" component={Signup} />
+        <PublicRoute exact path="/login" component={Login} />
+        <PrivateRoute exact path="/private" component={Private} />
+      </Switch>
     </div>
   );
 }
