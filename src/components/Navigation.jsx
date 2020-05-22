e7f33b6debe1ae1145734ee4d86217794fbfcd5e
Jacopo <jacopo.rodighiero@icloud.com>
2020-05-22 23:45:29 +0200

    friday 23:45 nearly navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,49 +1,54 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withAuth } from '../lib/Auth'
-import { ThemeProvider } from 'emotion-theming'
-import theme from '@rebass/preset'
-import {Flex} from 'rebass'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withAuth } from "../lib/Auth";
+import { ThemeProvider } from "emotion-theming";
+import theme from "@rebass/preset";
+import { Flex } from "rebass";
 class Navigation extends Component {
   render() {
-    // `user`, `logout`, `isLoggedIn` are coming from the AuthProvider 
+    // `user`, `logout`, `isLoggedIn` are coming from the AuthProvider
     // and are injected by the withAuth HOC
     const { user, logout, isLoggedIn } = this.props;
 
     return (
       <ThemeProvider theme={theme}>
-      <Flex className="navbar">
-        <Link to={'/'} id='home-btn'>
-          <h4>Home</h4>
-        </Link>
-        {
-          isLoggedIn
-            ? <div className="navbar-btns">
-              <Link to={'/profile'} id='profile-btn'>
+        <Flex
+          px={1}
+          color="white"
+          bg="black"
+          alignItems="center"
+          className="navbar"
+        >
+          <Link to={"/"} id="home-btn">
+            <h4>LAVIKA</h4>
+          </Link>
+          {isLoggedIn ? (
+            <div className="navbar-btns">
+              <Link to={"/profile"} id="profile-btn">
                 <h4>{user.username}</h4>
               </Link>
-              <Link to={'/xp'} id='xp-btn'>
-              <h4>XP</h4>
-               </Link>
-              <Link to={'/comunity'} id='comunity-btn'>
-              <h4>COMUNITY</h4>
-               </Link>
-              <button onClick={logout}> Logout </button>
+              <Link to={"/xp"} id="xp-btn">
+                <h4>XP</h4>
+              </Link>
+              <Link to={"/comunity"} id="comunity-btn">
+                <h4>COMUNITY</h4>
+              </Link>
+              <h4 onClick={logout}> Logout </h4>
             </div>
-            : (
-              <>
-                <Link to="/login">
-                  {' '}
-                  <button className="navbar-button">Login</button>{' '}
-                </Link>
-                <br />
-                <Link to="/signup">
-                  {' '}
-                  <button className="navbar-button">Sign Up</button>{' '}
-                </Link>
-              </>
-            )}
-      </Flex>
+          ) : (
+            <>
+              <Link to="/login">
+                {" "}
+                <h4 className="navbar-button">Login</h4>{" "}
+              </Link>
+              <br />
+              <Link to="/signup">
+                {" "}
+                <h4 className="navbar-button">Sign Up</h4>{" "}
+              </Link>
+            </>
+          )}
+        </Flex>
       </ThemeProvider>
     );
   }
